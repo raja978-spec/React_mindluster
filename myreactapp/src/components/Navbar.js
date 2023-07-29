c236156219446d28b157222455bfba8bd56dd8c6
raja-spec <rraja48569@gmail.com>
2023-07-29 16:35:53 +0530

    Adding Container Component

--- myreactapp/src/components/Navbar.js
+++ myreactapp/src/components/Navbar.js
@@ -1,21 +1,23 @@
-import React ,{Component} from 'react'
 
-export default class Navbar extends Component{
-
-    render(){
-        const {name}=this.props;
-        return(
-            <div className='Nav'>
-        {   
-            name.map(data =>{
-            return(
-                <div className='Namelist' key={data.id}>
-                <h1>{data.name}</h1><br></br>
-                </div>
-                )})
-        }
-            </div>
+const Navbar=(props)=>{
+    const {name}=props;
+   
+    return(
+        <div className='Nav'>
         
-        )       
-    }
+        {   
+        name.map(data =>{
+        return(
+            <div className='Namelist' key={data.id}>
+            <h1>{data.name}</h1><br></br>
+            </div>
+            )})
+        }
+        
+        </div>
+    
+    )       
+    
 }
+
+export default Navbar
